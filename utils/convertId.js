8fe6e92c04b63901e0892c33fd15b2606c54c172
anilpdv <pdvanil007@gmail.com>
2019-06-27 22:57:20 +0530

    adding utils

---
+++ utils/convertId.js
@@ -0,0 +1,29 @@
+const test = length => {
+  let possible;
+  let randomChar = "";
+
+  possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk    lmnopqrstuvwxyz";
+
+  for (let i = 0; i < length; i++) {
+    randomChar =
+      randomChar + possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return randomChar;
+};
+
+const convertId = videoId => {
+  let charid;
+  let length;
+  let generatedValue = "";
+
+  for (let i = 0; i < videoId.length; i++) {
+    charid = videoId.charCodeAt(i) + 13;
+
+    length = Math.floor(Math.random() * 3 + 1);
+
+    generatedValue = generatedValue + (charid + test(length));
+  }
+  return generatedValue;
+};
+
+module.exports = convertId;
---
+++ utils/urls.js
@@ -0,0 +1,13 @@
+const urls = {
+  key: '',
+
+  baseUrl: 'https://www.googleapis.com/youtube/v3',
+};
+
+module.exports = urls;
+//   '',
+//   '',
+//   ,
+//   ''
+////
+//AIzaSyB8hIut_bsW-lQxiWMPsh7A7Xhk_OLFm9o
