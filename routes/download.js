5b9813313a23b5e5a222de0d87de03847e997dfc
anilpdv <pdvanil007@gmail.com>
2019-06-30 22:58:39 +0530

    changing download video

--- routes/download.js
+++ routes/download.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const axios = require("axios");
-const ytdl = require("youtube-dl");
-const fs = require("fs");
+const express = require('express');
+const axios = require('axios');
+const ytdl = require('youtube-dl');
+const fs = require('fs');
 
 const router = express.Router();
 
-router.get("/download/:id/song/:name", (req, res) => {
-  const video = ytdl("https://www.youtube.com/watch?v=" + req.params.id, [
-    "--format=bestaudio"
+router.get('/download/:id/song/:name', (req, res) => {
+  const video = ytdl('https://www.youtube.com/watch?v=' + req.params.id, [
+    '--format=140',
   ]);
-  res.setHeader("Content-Type", "audio/mpeg");
+  res.setHeader('Content-Type', 'audio/mpeg');
   res.setHeader(
-    "Content-Disposition",
-    "attachment; filename=" + req.params.name + ".webm"
+    'Content-Disposition',
+    'attachment; filename=' + req.params.name + '.m4a',
   );
-  video.on("error", err => console.log(err));
+  video.on('error', err => console.log(err));
   video.pipe(res);
 });
 
