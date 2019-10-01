5fd1ea51191c9ec85c21b78820021889b8c5d619
anilpdvmsr <anil.palli@msr-it.com>
2019-10-01 06:29:14 -0700

    adding error boundary

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
 // : loading the modules needed
-const express = require('express');
-const expressOasGenerator = require('express-oas-generator');
-const path = require('path');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const expressOasGenerator = require("express-oas-generator");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
 
 // : routes
-const searchRoute = require('./routes/search');
-const downloadRoute = require('./routes/download');
-const lyricsRoute = require('./routes/lyrics');
-const listenRoute = require('./routes/listen');
-const relatedRoute = require('./routes/relatedSongs.js');
-const playlistRoute = require('./routes/playlist.js');
+const searchRoute = require("./routes/search");
+const downloadRoute = require("./routes/download");
+const lyricsRoute = require("./routes/lyrics");
+const listenRoute = require("./routes/listen");
+const relatedRoute = require("./routes/relatedSongs.js");
+const playlistRoute = require("./routes/playlist.js");
 
 const app = express();
 
 // : oas setup
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 expressOasGenerator.init(app, {});
 app.use(cors());
 
 // : middle ware
-app.use('/api', searchRoute);
-app.use('/api', downloadRoute);
-app.use('/api', lyricsRoute);
-app.use('/api', listenRoute);
-app.use('/api', relatedRoute);
-app.use('/api', playlistRoute);
+app.use("/api", searchRoute);
+app.use("/api", downloadRoute);
+app.use("/api", lyricsRoute);
+app.use("/api", listenRoute);
+app.use("/api", relatedRoute);
+app.use("/api", playlistRoute);
 
 // : listening to the port
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log('server is started and listening on the port 3000');
+  console.log("server is started and listening on the port 3000");
+});
+
+var airbrake = new AirbrakeClient({
+  projectId: 245886,
+  projectKey: "b1729b17ff8bda4c50f40b9b4ec029b6"
 });
